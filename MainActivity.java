4689f8b5988181e98afc6e1b618dd2c2d17cd26b
shaheerzaidi <50184254+shaheerzaidi@users.noreply.github.com>
2020-02-17 20:10:56 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -136,8 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void resetBoard() {
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++)
-            {
+            for (int j = 0; j < 3; j++) {
                 buttons[i][j].setText("");
             }
         }
