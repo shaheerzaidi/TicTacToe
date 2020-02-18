7e6e3b0294551af8ddc5d31498487ee9e66d82eb
shaheerzaidi <50184254+shaheerzaidi@users.noreply.github.com>
2020-02-17 19:35:18 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         getTextViewPlayer2 = findViewById(R.id.text_view_p2);
 
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             for(int j = 0; j < 3; j++)
             {
