595efa545b27355cb6147e2a61934ce7c9ff1cc2
shaheerzaidi <50184254+shaheerzaidi@users.noreply.github.com>
2020-02-17 20:09:40 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return false;
     }
 
-    private  void player1Wins() {
+    private void player1Wins() {
         player1Points++;
         Toast.makeText(this, "Player 1 wins", Toast.LENGTH_SHORT).show();
         updatePointsText();
@@ -129,16 +129,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetBoard();
     }
 
-    private void updatePointsText()
-    {
+    private void updatePointsText() {
         textViewPlayer1.setText("Player 1: " + player1Points);
         getTextViewPlayer2.setText("Player 2: " + player2Points);
     }
 
-    private void resetBoard()
-    {
+    private void resetBoard() {
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j ++)
+            for (int j = 0; j < 3; j++)
             {
                 buttons[i][j].setText("");
             }
