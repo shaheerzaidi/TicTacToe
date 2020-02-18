0f0e9c2b0f712449225ed15415798811ba4541e6
shaheerzaidi <50184254+shaheerzaidi@users.noreply.github.com>
2020-02-17 19:50:55 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -27,13 +27,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         getTextViewPlayer2 = findViewById(R.id.text_view_p2);
 
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 String buttonID = "Button_" + i + j;
                 int resId = getResources().getIdentifier(buttonID, "id", getPackageName());
-                buttons [i][j] = findViewById(resId);
+                buttons[i][j] = findViewById(resId);
                 buttons[i][j].setOnClickListener(this);
             }
         }
@@ -49,26 +47,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (!(((Button) v).getText().toString().equals("")))
-        {
+        if (!(((Button) v).getText().toString().equals(""))) {
             return;
         }
 
-        if(player1Turn)
-        {
+        if (player1Turn) {
             ((Button) v).setText("X");
         }
 
-        else
-        {
+        else {
             ((Button) v).setText("O");
         }
 
         roundCount++;
 
-        if (checkForWin())
-        {
-            if (player1Turn){
+        if (checkForWin()) {
+            if (player1Turn) {
                 player1Wins();
             }
 
@@ -77,52 +71,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         
-        else if (roundCount == 9)
-        {
+        else if (roundCount == 9) {
             draw();
         }
 
-        else
-            {
-                player1Turn = !player1Turn;
-            }
+        else {
+            player1Turn = !player1Turn;
+        }
     }
 
     private boolean checkForWin()
     {
         String [][] field = new String[3][3];
 
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 field [i][j] = buttons[i][j].getText().toString();
-
             }
         }
 
-        for (int i = 0; i < 3; i++)
-        {
-            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals(""))
-            {
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
                 return true;
             }
 
-            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals(""))
-            {
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
                 return true;
             }
 
-            if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals(""))
-            {
+            if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
                 return true;
             }
 
-            if (field[0][2].equals(field[2][2]) && field[0][0].equals(field[2][0]) && !field[0][2].equals(""))
-            {
+            if (field[0][2].equals(field[2][2]) && field[0][0].equals(field[2][0]) && !field[0][2].equals("")) {
                 return true;
             }
-
         }
         return false;
     }
@@ -150,12 +133,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         textViewPlayer1.setText("Player 1: " + player1Points);
         getTextViewPlayer2.setText("Player 2: " + player2Points);
-
     }
 
     private void resetBoard()
     {
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j ++)
             {
                 buttons[i][j].setText("");
